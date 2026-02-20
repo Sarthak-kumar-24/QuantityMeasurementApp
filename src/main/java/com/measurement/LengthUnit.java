95bb98cbfe991bc365fda6f4cbc9ce752f60ef54
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-02-20 22:08:06 +0530

    [Sarthak]: modified QuantityMeasurementAppTest class for UC3

---
+++ src/main/java/com/measurement/LengthUnit.java
@@ -0,0 +1,31 @@
+package com.measurement;
+
+/**
+ * Enum representing supported length units.
+ * Base unit chosen: INCH
+ */
+public enum LengthUnit {
+
+    FEET(12.0),
+    INCH(1.0);
+
+    private final double conversionFactorToInch;
+
+    LengthUnit(double conversionFactorToInch) {
+        this.conversionFactorToInch = conversionFactorToInch;
+    }
+
+    /**
+     * Converts a value of this unit to base unit (inch)
+     */
+    public double toBaseUnit(double value) {
+        return value * conversionFactorToInch;
+    }
+
+    /**
+     * Parses user input safely to LengthUnit
+     */
+    public static LengthUnit fromString(String unit) {
+        return LengthUnit.valueOf(unit.toUpperCase());
+    }
+}
