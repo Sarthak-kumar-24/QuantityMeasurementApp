5f210e83dcd625b9e9ee65606a318619b65f3322
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-02-20 21:14:52 +0530

    [Sarthak]: added Inche class for UC2

---
+++ src/main/java/com/measurement/Inches.java
@@ -0,0 +1,11 @@
+package com.measurement;
+
+/**
+ * Represents an Inches measurement.
+ */
+public class Inches extends Measurement {
+
+    public Inches(double value) {
+        super(value);
+    }
+}
