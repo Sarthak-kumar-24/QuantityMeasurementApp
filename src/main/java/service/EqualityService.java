ab1a44e1fa57306e90247fa3309e879475c915fc
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-02-23 14:51:43 +0530

    [Sarthak]: modified EqualityService class for UC9

--- src/main/java/service/EqualityService.java
+++ src/main/java/service/EqualityService.java
@@ -1,15 +1,14 @@
 package service;
 
-/**
- * Generic equality service.
- */
 public class EqualityService {
 
     public boolean areEqual(Object o1, Object o2) {
-
         if (o1 == o2) return true;
         if (o1 == null || o2 == null) return false;
 
+        if (!o1.getClass().equals(o2.getClass())) {
+            return false; // Length ≠ Weight
+        }
         return o1.equals(o2);
     }
 }
