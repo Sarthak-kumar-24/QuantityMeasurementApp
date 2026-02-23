344c4e826b8f69f4991e1e708623fdfc862a30e6
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-02-23 15:22:31 +0530

    [Sarthak]: added IMeasurable class for UC10

---
+++ src/main/java/com/measurement/IMeasurable.java
@@ -0,0 +1,19 @@
+package com.measurement;
+
+/**
+ * UC10: Common interface for all measurable units
+ */
+public interface IMeasurable {
+
+    double getConversionFactor();
+
+    default double convertToBaseUnit(double value) {
+        return value * getConversionFactor();
+    }
+
+    default double convertFromBaseUnit(double baseValue) {
+        return baseValue / getConversionFactor();
+    }
+
+    String getUnitName();
+}
