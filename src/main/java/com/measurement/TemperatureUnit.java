dabd900f0cee0655c3fefe383f60ddaef470c9ac
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-02-25 10:54:09 +0530

    [Sarthak]: added TemperatureUnit class

---
+++ src/main/java/com/measurement/TemperatureUnit.java
@@ -0,0 +1,56 @@
+package com.measurement;
+
+
+public enum TemperatureUnit implements IMeasurable {
+
+    CELSIUS("C"),
+    FAHRENHEIT("F"),
+    KELVIN("K");
+
+    private final String symbol;
+
+    TemperatureUnit(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public double getConversionFactor() {
+        return 1.0; // not used
+    }
+
+    @Override
+    public String getUnitName() {
+        return symbol;
+    }
+
+    @Override
+    public boolean supportsArithmetic() {
+        return false; // UC14 default restriction
+    }
+
+    @Override
+    public double convertToBaseUnit(double value) {
+        // Base = Celsius
+        return switch (this) {
+            case CELSIUS -> value;
+            case FAHRENHEIT -> (value - 32) * 5 / 9;
+            case KELVIN -> value - 273.15;
+        };
+    }
+
+    @Override
+    public double convertFromBaseUnit(double baseValue) {
+        return switch (this) {
+            case CELSIUS -> baseValue;
+            case FAHRENHEIT -> baseValue * 9 / 5 + 32;
+            case KELVIN -> baseValue + 273.15;
+        };
+    }
+
+    @Override
+    public void validateOperationSupport(String operation) {
+        throw new UnsupportedOperationException(
+            "Temperature does not support arithmetic operation: " + operation
+        );
+    }
+}
