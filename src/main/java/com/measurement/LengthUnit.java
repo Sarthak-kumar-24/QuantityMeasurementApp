e9a814eba9066182c1b222a04b3766f7b9208b3d
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-03-09 21:02:50 +0530

    [Sarthak]: modified LengthUnit for UC14

--- src/main/java/com/measurement/LengthUnit.java
+++ src/main/java/com/measurement/LengthUnit.java
@@ -1,11 +1,26 @@
 package com.measurement;
 
+/*
+ * LengthUnit enum represents different length measurement units.
+ *
+ * This enum implements the IMeasurable interface so that
+ * all length units follow the same conversion contract.
+ *
+ * Base Unit: FEET
+ *
+ * All conversion factors are defined relative to FEET.
+ *
+ * Example conversions:
+ * 1 foot  = 12 inches
+ * 1 yard  = 3 feet
+ * 1 foot  = 30.48 cm
+ */
 public enum LengthUnit implements IMeasurable {
 
-    FEET(1.0),
-    INCHES(1.0 / 12.0),
-    YARDS(3.0),
-    CENTIMETERS(1.0 / 30.48);
+    FEET(1.0),                // Base unit
+    INCHES(1.0 / 12.0),       // 1 inch = 1/12 feet
+    YARDS(3.0),               // 1 yard = 3 feet
+    CENTIMETERS(1.0 / 30.48); // 1 cm = 1/30.48 feet
 
     private final double toFeet;
 
@@ -13,16 +28,40 @@ public enum LengthUnit implements IMeasurable {
         this.toFeet = toFeet;
     }
 
+    /*
+     * Returns the conversion factor relative to the base unit.
+     *
+     * This method overrides the abstract method from IMeasurable.
+     */
     @Override
     public double getConversionFactor() {
         return toFeet;
     }
 
+    /*
+     * Returns the name of the unit.
+     *
+     * Example:
+     * FEET, INCHES, YARDS, CENTIMETERS
+     */
     @Override
     public String getUnitName() {
         return name();
     }
 
+    /*
+     * Converts a string input to the corresponding LengthUnit.
+     *
+     * This method allows flexible input from users.
+     *
+     * Supported inputs:
+     * ft, feet -> FEET
+     * in, inch -> INCHES
+     * yd, yard -> YARDS
+     * cm, centimeter -> CENTIMETERS
+     *
+     * If the unit is invalid, an exception is thrown.
+     */
     public static LengthUnit fromString(String unit) {
         return switch (unit.trim().toLowerCase()) {
             case "ft", "feet" -> FEET;
