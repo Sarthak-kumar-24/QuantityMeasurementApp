1eddb722cc4f0537a4c4b6fdf09420fcf89f7822
Sarthak-kumar-24 <sarthaksingh1442@gmail.com>
2026-03-09 21:05:17 +0530

    [Sarthak]: modified WeightUnit for UC14

--- src/main/java/com/measurement/WeightUnit.java
+++ src/main/java/com/measurement/WeightUnit.java
@@ -1,17 +1,54 @@
 package com.measurement;
 
+
+/*
+ * =========================================================
+ * WeightUnit Enum
+ * =========================================================
+ *
+ * Represents supported weight measurement units.
+ *
+ * Base Unit: KILOGRAM
+ *
+ * Each unit defines a conversion factor relative
+ * to kilograms.
+ *
+ * Example conversions:
+ * 1 kilogram = 1000 grams
+ * 1 pound    = 0.453592 kilograms
+ *
+ * =========================================================
+ * UC11 – Weight Measurement Support
+ * =========================================================
+ *
+ * Weight units implement the IMeasurable interface,
+ * allowing them to integrate with the generic Quantity
+ * class for:
+ *
+ * - unit conversion
+ * - equality comparison
+ * - arithmetic operations
+ */
 public enum WeightUnit implements IMeasurable {
 
-    KILOGRAM(1.0),
-    GRAM(0.001),
-    POUND(0.453592);
-
+    KILOGRAM(1.0),     // Base unit
+    GRAM(0.001),       // 1 g = 0.001 kg
+    POUND(0.453592);   // 1 lb = 0.453592 kg
+	
+	
+	/*
+     * Conversion factor relative to kilograms.
+     */
     private final double toKg;
 
     WeightUnit(double toKg) {
         this.toKg = toKg;
     }
-
+    
+    
+    /*
+     * Returns the conversion factor relative to the base unit.
+     */
     @Override
     public double getConversionFactor() {
         return toKg;
@@ -21,7 +58,16 @@ public enum WeightUnit implements IMeasurable {
     public String getUnitName() {
         return name();
     }
-
+    
+    
+    /*
+     * Converts user input string to WeightUnit.
+     *
+     * Supported inputs:
+     * kg -> KILOGRAM
+     * g  -> GRAM
+     * lb -> POUND
+     */
     public static WeightUnit fromString(String unit) {
         return switch (unit.trim().toLowerCase()) {
             case "kg" -> KILOGRAM;
